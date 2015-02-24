12080768d64033500b168114cc4a8c689e49d22e
Pietro Inglese <magicenglish93@gmail.com>
2015-02-24 16:05:23 +0100

    Initiate program

---
+++ HelloGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("HelloGit!!");
+            System.Console.WriteLine("Newline");
+            System.Console.WriteLine("Another Newline");
+        }
+    }
+}
